2e77c6fbace56ff3a72549bc3d675da7df27f2a8
Ansonsd5 <anson11sd@gmail.com>
2024-04-11 16:12:52 +0530

    fix: usage of express-validation

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 import express, { json, request, response } from "express";
+import { body, query, validationResult } from "express-validator";
 
 const app = express();
 
@@ -38,18 +39,29 @@ app.get("/", (req, res) => {
   res.status(201).send(`<h1>Welcome to my page have a great time</h1>`);
 });
 
-app.get("/api/users", (req, res) => {
-  const {
-    query: { filter, value },
-  } = req;
-  //when filter and value are undefined
+app.get(
+  "/api/users",
+  query("filter")
+    .notEmpty()
+    .withMessage("Cant be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be 3-10 char"),
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result.errors);
 
-  if (filter && value) {
-    return res.send(mockData.filter((user) => user[filter].includes(value)));
+    const {
+      query: { filter, value },
+    } = req;
+    //when filter and value are undefined
+
+    if (filter && value) {
+      return res.send(mockData.filter((user) => user[filter].includes(value)));
+    }
+
+    return res.send(mockData);
   }
-
-  return res.send(mockData);
-});
+);
 
 app.use((req, res, next) => {
   console.log(
@@ -77,32 +89,48 @@ app.get("/api/products", (request, response) => {
   ]);
 });
 
-app.post("/api/users", (request, response) => {
-  const { body } = request;
-  const newuser = { id: mockData[mockData.length - 1].id + 1, ...body };
-  mockData.push(newuser);
-  return response.status(201).send(mockData);
-});
+app.post(
+  "/api/users",
+  [body("username")
+    .notEmpty()
+    .withMessage("payload cant be empty")
+    .isString()
+    .withMessage("Please enter a valid string"),
+  body("displayname")
+    .notEmpty()
+    .withMessage("displayname cant be empty")
+    .isString()
+    .withMessage("Please enter a valid string")
+    .isLength({ max: 32 })
+    .withMessage("max length should be 32")],
+  (request, response) => {
+    const postresult = validationResult(request);
+    console.log(postresult);
 
-app.put("/api/users/:id",resolveIndexByUserId, (request, response) => {
-  const {
-    body,findUser
-  } = request;
+    if(!postresult.isEmpty()){
+      return response.status(400).send({errors: postresult.array()})
+    }
+    const { body } = request;
+    const newuser = { id: mockData[mockData.length - 1].id + 1, ...body };
+    mockData.push(newuser);
+    return response.status(201).send(mockData);
+  }
+);
+
+app.put("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUser } = request;
   mockData[findUser] = { id: mockData[findUser].id, ...body };
   return response.status(201).send(mockData);
 });
 
-app.patch("/api/users/:id",resolveIndexByUserId, (request, response) => {
-  const {
-    body,
-    findUser,
-  } = request;
+app.patch("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { body, findUser } = request;
   mockData[findUser] = { ...mockData[findUser], ...body };
   return response.status(200).send(mockData);
 });
 
-app.delete("/api/users/:id",resolveIndexByUserId, (request, response) => {
-const {findUser } =request;
+app.delete("/api/users/:id", resolveIndexByUserId, (request, response) => {
+  const { findUser } = request;
 
   mockData.splice(findUser);
   return response.status(200).send(mockData);
